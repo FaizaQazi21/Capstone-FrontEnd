0c8b8028c01d3bef81d520875dc100d5c5a023a2
Ricardo Vaz <ricardodaniel.vaz@gmail.com>
2021-11-09 15:18:06 +0000

    admin view

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,9 +1,11 @@
 import ExampleLandingAdmin from "../components/LandingAdmin";
-
+import UserView from "../components/ProjectView";
+import ListView from "../components/ProjectList";
 export default function Admin() {
     return (
       <>
         <ExampleLandingAdmin/>
+        <UserView/>
       </>
     );
   }
